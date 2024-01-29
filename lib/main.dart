ee9f1a907207ddfb86c0cbc63eebdebb3cb415a7
Sidessh <mosiddhesh@gmail.com>
2024-01-29 22:57:58 +0530

    Added Bottom Nav Bar + Sample-Routes + Profile Page Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:aqua_trace/aqua_trace.dart';
+import 'package:aqua_trace/blog.dart';
+import 'package:aqua_trace/charts.dart';
 import 'package:aqua_trace/login.dart';
+import 'package:aqua_trace/profile_page.dart';
 import 'package:aqua_trace/register.dart';
+import 'package:aqua_trace/share.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: 'login',
     routes: {
       'login': (context) => Login(),
       'register': (context) => Register(),
+      'profile': (context) => ProfilePage(),
+      'blog': (context) => BlogPage(),
+      'aqua_trace': (context) => AquaTrace(),
+      'charts': (context) => ChartsPage(),
+      'share': (context) => Share(),
     },
   ));
 }
---
+++ lib/share.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Share extends StatelessWidget {
+  const Share({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            'Share',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 4,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, 'share');
+                },
+                child: const Icon(
+                  CupertinoIcons.share,
+                  size: 25,
+                ),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, 'charts');
+                },
+                child: const Icon(
+                  CupertinoIcons.chart_bar_alt_fill,
+                  size: 25,
+                ),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 24, 94, 247),
+                radius: 25,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, 'aqua_trace');
+                  },
+                  child: const Icon(
+                    CupertinoIcons.drop_fill,
+                    color: Colors.white,
+                    size: 33,
+                  ),
+                ),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, 'blog');
+                },
+                child: const Icon(
+                  CupertinoIcons.book_fill,
+                  size: 25,
+                ),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, 'profile');
+                },
+                child: const Icon(
+                  CupertinoIcons.person,
+                  size: 25,
+                ),
+              ),
+              label: '',
+            )
+          ]),
+    );
+  }
+}
