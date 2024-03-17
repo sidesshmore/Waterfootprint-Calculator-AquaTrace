5ecc95f9a56e16de0b44747951418908a85c4d85
Sidessh <mosiddhesh@gmail.com>
2024-03-17 13:32:28 +0530

    date picker added

--- lib/features/Aqua_Trace/widgets/topBar.dart
+++ lib/features/Aqua_Trace/widgets/topBar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class TopBar extends StatefulWidget {
   const TopBar({super.key});
@@ -8,132 +10,58 @@ class TopBar extends StatefulWidget {
 }
 
 class _TopBarState extends State<TopBar> {
-  bool todaySelected = true;
-  bool thisweekSelected = false;
-  bool thismonthSelected = false;
-  bool sixmonthSelected = false;
+  DateTime _selectedDate = DateTime.now(); // Track selected date
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    String buttonText = DateFormat('dd MMM, yyyy')
+        .format(_selectedDate); // Format the selected date as "17 Nov, 2024"
+
     return Container(
       height: 40,
       margin: const EdgeInsets.only(top: 16),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            margin: const EdgeInsets.only(left: 8),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  todaySelected = true;
-                  thisweekSelected = false;
-                  thismonthSelected = false;
-                  sixmonthSelected = false;
-                });
-              },
-              style: TextButton.styleFrom(
-                  backgroundColor: todaySelected
-                      ? const Color.fromARGB(255, 24, 94, 247)
-                      : const Color.fromRGBO(216, 216, 216, 1),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: Text(
-                'Today',
-                style: TextStyle(
-                    color: todaySelected ? Colors.white : Colors.black,
-                    fontSize: 15),
-              ),
+          SizedBox(
+            width: screenWidth / 10,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              buttonText,
+              style: TextStyle(fontSize: 19),
             ),
           ),
-          const SizedBox(width: 5),
-          Container(
-            margin: const EdgeInsets.only(left: 8),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  todaySelected = false;
-                  thisweekSelected = true;
-                  thismonthSelected = false;
-                  sixmonthSelected = false;
-                });
-              },
-              style: TextButton.styleFrom(
-                  backgroundColor: thisweekSelected
-                      ? const Color.fromRGBO(52, 104, 192, 1)
-                      : const Color.fromRGBO(216, 216, 216, 1),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: Text(
-                'This Week',
-                style: TextStyle(
-                    color: thisweekSelected ? Colors.white : Colors.black,
-                    fontSize: 15),
-              ),
+          InkWell(
+            onTap: () {
+              _showDatePicker(context);
+            },
+            child: CircleAvatar(
+              child: Icon(Icons.calendar_month_outlined),
             ),
-          ),
-          const SizedBox(width: 5),
-          Container(
-            margin: const EdgeInsets.only(left: 8),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  todaySelected = false;
-                  thisweekSelected = false;
-                  thismonthSelected = true;
-                  sixmonthSelected = false;
-                });
-              },
-              style: TextButton.styleFrom(
-                  backgroundColor: thismonthSelected
-                      ? const Color.fromRGBO(52, 104, 192, 1)
-                      : const Color.fromRGBO(216, 216, 216, 1),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: Text(
-                'This month',
-                style: TextStyle(
-                    color: thismonthSelected ? Colors.white : Colors.black,
-                    fontSize: 15),
-              ),
-            ),
-          ),
-          const SizedBox(width: 5),
-          Container(
-            margin: const EdgeInsets.only(left: 8),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  todaySelected = false;
-                  thisweekSelected = false;
-                  thismonthSelected = false;
-                  sixmonthSelected = true;
-                });
-              },
-              style: TextButton.styleFrom(
-                  backgroundColor: sixmonthSelected
-                      ? const Color.fromRGBO(52, 104, 192, 1)
-                      : const Color.fromRGBO(216, 216, 216, 1),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: Text(
-                '6 Months',
-                style: TextStyle(
-                    color: sixmonthSelected ? Colors.white : Colors.black,
-                    fontSize: 15),
-              ),
-            ),
-          ),
-          const SizedBox(width: 5),
+          )
         ],
       ),
     );
+  }
+
+  // Function to show the date picker
+  Future<void> _showDatePicker(BuildContext context) async {
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null && pickedDate != _selectedDate) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
   }
 }
